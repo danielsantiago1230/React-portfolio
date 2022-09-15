906b1065bce9f0bf32bffa5f1edac3ab42e06c62
santiagoquintero95 <danielsantiago1230@gmail.com>
2022-09-15 15:52:23 -0500

    add tailwind-sucessfull

--- my-portfolio/src/index.js
+++ my-portfolio/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Santiago from './Santiago';
+import App from './App.jsx';
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Santiago />
+    <App />
   </React.StrictMode>
 );
 
